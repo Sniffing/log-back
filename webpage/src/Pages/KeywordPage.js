447e21143239b6b50b852ece2072898d14a0a79a
Terence <terence@Terences-MacBook-Air.local>
2019-04-16 00:51:42 -0700

    Add keywords page

---
+++ webpage/src/Pages/KeywordPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KeywordsCount from '../Charts/KeywordsCount';
+
+const KeywordPage = () => {
+  return (
+    <div className='keyword-page'>
+      <KeywordsCount />
+    </div>
+  );
+}
+
+export default KeywordPage;
--- webpage/src/Pages/Router.js
+++ webpage/src/Pages/Router.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 
 const Router = () => (
   <div>
-    <Link to='/'> Home </Link>
-    <Link to='/weight'> Weight! </Link>
+    <Link to='/'>
+      <Card>
+        <p> Home </p>
+      </Card>
+    </Link>
+    <Link to='/weight'>
+      <Card>
+        <p> Weight </p>
+      </Card>
+    </Link>
+    <Link to='/keywords'>
+      <Card>
+        <p> Words </p>
+      </Card>
+    </Link>
   </div>
 );
 
