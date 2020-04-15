24ea19356434effe9d050f26db9acf7759e66b75
Terence <terence@Terences-MacBook-Air.local>
2020-04-15 00:11:30 -0700

    moves to server file, adds typescript, adds test

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,13 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import { join } from 'path';
 import express from 'express';
+import { Request, Response } from 'express';
 import { json, urlencoded } from 'body-parser';
-import multer from 'multer';
 import { enableAll, info, error } from 'loglevel';
 import { Datastore } from '@google-cloud/datastore';
+import { ILogEntry, IWeightDTO, IKeywordDTO, ITextDTO, ILogEntryDTO, BooleanMetric, ILogEntryData } from './interfaces';
 
 dotenv.config();
-const upload = multer();
 const app = express();
 const port = 4000;
 enableAll();
@@ -26,9 +26,9 @@ app.get('/', function(req,res) {
   res.sendFile(join(__dirname,'/web-page/public/','index.html'));
 })
 
-app.post('/', upload.array(), function (req, res, next) {
+app.post('/', function (req, res, next) {
   const body = req.body;
-  const data = formatData(body);
+  const data: ILogEntryDTO = formatData(body);
   saveIfDoesNotExist(data, res);
 })
 
@@ -55,13 +55,16 @@ app.listen(port, () => {
   info(`Be sure to run 'ngrok http ${port}'`);
 })
 
-app.get('/weight', function(req,res) {
-  let result = [];
+app.get('/weight', (req: Request, res: Response) => {
+  const result: IWeightDTO[] = [];
   const query = datastore.createQuery(kind)
                          .filter('weight', ">", "0");
   query.run((err, entities) => {
     for (var i = 0; i < entities.length; i++) {
-      result.push({date: entities[i].date, weight: entities[i].weight});
+      result.push({
+        date: entities[i].date, 
+        weight: entities[i].weight
+      });
     }
     res.send(result);
   });
@@ -69,12 +72,14 @@ app.get('/weight', function(req,res) {
 
 app.get('/keywords', function(req,res) {
   const query = datastore.createQuery(kind);
-  let result = [];
+  let result: IKeywordDTO[] = [];
   query.run((err, entities) => {
     console.log("There were %s entities retrieved", entities.length);
-
     for (var i = 0; i < entities.length; i++) {
-      result.push({"date": entities[i].date, "keywords": entities[i].keywords})
+      result.push({
+        date: entities[i].date, 
+        keywords: entities[i].keywords
+      });
     }
     res.send(result);
   });
@@ -82,7 +87,7 @@ app.get('/keywords', function(req,res) {
 
 app.get('/text', function(req,res) {
   const query = datastore.createQuery(kind);
-  let result = [];
+  let result: ITextDTO[] = [];
   query.run((err, entities) => {
     console.log("There were %s entities retrieved", entities.length);
 
@@ -93,7 +98,7 @@ app.get('/text', function(req,res) {
   });
 });
 
-let saveIfDoesNotExist = (data, res) => {
+let saveIfDoesNotExist = (data: ILogEntryDTO, res: Response) => {
   datastore.get(data.key)
   .then((entity) => {
     if (entity.length < 1 || (entity.length === 1 && entity[0] == undefined)) {
@@ -109,10 +114,10 @@ let saveIfDoesNotExist = (data, res) => {
   });
 }
 
-const metricsToList = (jsonState) => {
+const metricsToList = (jsonState: Partial<Record<BooleanMetric, boolean>>) => {
   var list = [];
   for (var key in jsonState) {
-    if (jsonState[key]) {
+    if (jsonState[key as BooleanMetric]) {
       list.push(key.toLowerCase());
     }
   }
@@ -120,13 +125,13 @@ const metricsToList = (jsonState) => {
   return list;
 }
 
-const reverseDate = (date) => {
+const reverseDate = (date: string) => {
   let parts = date.split("-");
   return parts[2] + "-" + parts[1] + "-" + parts[0];
 }
 
-const formatData = (state) => {
-  const data = {
+const formatData = (state: ILogEntry): ILogEntryDTO => {
+  const data: ILogEntryDTO = {
     key: datastore.key([kind, reverseDate(state.dateState.date)]),
     data: {
       date: reverseDate(state.dateState.date),
@@ -139,7 +144,7 @@ const formatData = (state) => {
   return data;
 }
 
-const saveToCloud = (data, res) => {
+const saveToCloud = (data: ILogEntryDTO, res: Response) => {
   datastore.save(data)
   .then(() => {
     info("Saved data with key: ", data.key);
