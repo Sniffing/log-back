d8958f7649d6e0e1d0b04d73fec656f15bf0e77b
Terence <terence@Terences-MacBook-Air.local>
2020-04-15 20:37:04 -0700

    moves webpage out

--- web-page/src/App.interfaces.ts
+++
@@ -1,3 +0,0 @@
-export interface dummy {
-  dummy: string;
-}
--- web-page/src/App.utils.ts
+++
@@ -1,6 +0,0 @@
-export class Utils {
-    public static unixTimeToDate(time: number) {
-        var date = new Date(time);        
-        return `${('0' + date.getDate()).slice(-2)}-${('0' + date.getMonth()+1).slice(-2)}-${date.getFullYear()}`;
-    }
-}
